e0ee46cc6a2d21fc6a78906bc80f1e6c27ae79ea
Viren-55 <52177992+Viren-55@users.noreply.github.com>
2020-04-24 21:06:42 +0530

    Update bioinfo_prac4_question_5 - Copy.py

--- bioinfo_prac4_question_5 - Copy.py
+++ bioinfo_prac4_question_5 - Copy.py
@@ -13,14 +13,14 @@ def common_pentapeptide_between_org(x,y):
         penta_x.append(x[i:i+5])
         penta_y.append(y[i:i+5])
     common = list(set(penta_y).intersection(penta_x))
-    print(common)
-    print("Occurrences_common pentapeptides in chicken_hemoglobin")
+    #print(common)
+    #print("Occurrences_common pentapeptides in chicken_hemoglobin")
     for i in range(len(common)):
         print(common[i],penta_y.count(common[i]))
     
     print("Occurrences_common pentapeptides in human_hemoglobin")
     for i in range(len(common)):
-        print(common[i],penta_x.count(common[i]))
+        #print(common[i],penta_x.count(common[i]))
 human='MVHLTPEEKSAVTALWGKVNVDEVGGEALGRLLVVYPWTQRFFESFGDLSTPDAVMGNPKVKAHGKKVLGAFSDGLAHLDNLKGTFATLSELHCDKLHVDPENFRLLGNVLVCVLAHHFGKEFTPPVQAAYQKVVAGVANALAHKYH'
 chicken='MVHWTAEEKQLITGLWGKVNVAECGAEALARLLIVYPWTQRFFASFGNLSSPTAILGNPMVRAHGKKVLTSFGDAVKNLDNIKNTFSQLSELHCDKLHVDPENFRLLGDILIIVLAAHFSKDFTPECQAAWQKLVRVVAHALARKYH'
 
